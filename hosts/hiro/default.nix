054e9fd851b06e65fb6450b8046abdf4b35489a2
LeoQiao18 <qiaofeitong@hotmail.com>
2022-07-27 12:11:36 -0400

    add hiro

---
+++ hosts/hiro/default.nix
@@ -0,0 +1,97 @@
+{ pkgs, config, lib, ... }: {
+  imports = [ ./hardware-configuration.nix ];
+
+  modules = {
+    hardware = {
+      audio.enable = true;
+      bluetooth.enable = true;
+      logitech.enable = true;
+      printer.enable = true;
+      touchpad.enable = true;
+    };
+
+    networking = {
+      enable = true;
+      networkManager.enable = true;
+    };
+
+    themes.active = "nord";
+
+    desktop = {
+      xmonad.enable = true;
+      terminal = {
+        default = "kitty";
+        kitty.enable = true;
+      };
+      editors = {
+        default = "nvim";
+        neovim.enable = true;
+        emacs.enable = true;
+      };
+      browsers = {
+        default = "qutebrowser";
+        qutebrowser.enable = true;
+        firefox.enable = true;
+        chromium.enable = true;
+      };
+      media = {
+        social = { common.enable = true; };
+        viewer = {
+          video.enable = true;
+          music.enable = true;
+          document.enable = true;
+        };
+      };
+      extra = { dunst.enable = true; };
+      # virtual.wine.enable = true;
+    };
+
+    develop = {
+      cc.enable = true;
+      coq.enable = true;
+      diagnostic.enable = true;
+      go.enable = true;
+      haskell.enable = true;
+      javascript.enable = true;
+      lua.enable = true;
+      nix.enable = true;
+      ocaml.enable = true;
+      python.enable = true;
+      shell.enable = true;
+    };
+
+    # containers.transmission = {
+    #   enable = false; # TODO: Once fixed -> enable = true;
+    #   username = "alonzo";
+    #   password = builtins.readFile config.age.secrets.torBylon.path;
+    # };
+
+    # services = {
+    #   # ssh.enable = true;
+    #   kdeconnect.enable = true;
+    # };
+
+    shell = {
+      default = pkgs.zsh;
+      git.enable = true;
+      zsh.enable = true;
+      # fish.enable = true;
+      # gnupg.enable = true;
+    };
+  };
+
+  services = {
+    upower.enable = true;
+
+    # xserver = {
+    #   videoDrivers = ["amdgpu"];
+    #   deviceSection = ''
+    #     Option "TearFree" "true"
+    #   '';
+    #   libinput.touchpad = {
+    #     accelSpeed = "0.5";
+    #     accelProfile = "adaptive";
+    #   };
+    # };
+  };
+}
