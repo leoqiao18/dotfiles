61d1fe0f1a290c125209743499df13be705b3020
LeoQiao18 <qiaofeitong@hotmail.com>
2022-08-25 09:33:35 -0400

    add xss-lock

--- modules/desktop/extra/lockscreen.nix
+++ modules/desktop/extra/lockscreen.nix
@@ -4,11 +4,7 @@ with lib.my;
 let cfg = config.modules.desktop;
 in {
   config = mkIf cfg.xmonad.enable {
-    user.packages = with pkgs; [
-      i3lock
-      xautolock
-      #xss-lock
-    ];
+    user.packages = with pkgs; [ i3lock xautolock xss-lock ];
 
     # xautolock to lock the screen (using i3lock) after 1 minute
     # (including a notification 10 seconds before actually locking the screen)
@@ -18,8 +14,9 @@ in {
     services.xserver.displayManager.sessionCommands = ''
       xset s off -dpms
       ${getExe pkgs.xautolock} -detectsleep -time 5 \
-                -locker "${getExe pkgs.i3lock} -c 2e3440 && systemctl suspend" \
+                -locker "systemctl suspend" \
                 -notify 10 -notifier "${pkgs.libnotify}/bin/notify-send 'Suspending in 10 seconds'" &
+      ${getExe pkgs.xss-lock} -- ${getExe pkgs.i3lock} -c 2e3440 &
     '';
   };
 }
